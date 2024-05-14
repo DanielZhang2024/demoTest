509316811f5401ebaf6822e8ef1c5324f9cc8e71
danielZhang <xuanning_2024@qq.com>
2024-05-15 01:05:51 +0800

    113

--- src/main/java/com/tencent/wxcloudrun/service/UserService.java
+++ src/main/java/com/tencent/wxcloudrun/service/UserService.java
@@ -1,7 +1,15 @@
 package com.tencent.wxcloudrun.service;
 
+import com.tencent.wxcloudrun.model.ShopAccount;
+
+import java.util.Optional;
+
 public interface UserService {
 
     //用户登录，根据openid获取用户
+    Optional<ShopAccount> getAccount(String phone, String password);
 
+    void insertAccount(ShopAccount shopAccount);
+
+    void clearCount(Integer id);
 }
